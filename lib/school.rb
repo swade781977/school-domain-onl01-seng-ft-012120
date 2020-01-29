4296456e6bfed44a7cfe3c63c126ef2c551db49f
Chris Wade <swade781977@yahoo.com>
2020-01-29 22:42:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def name
+    @name
+  end
+  def roster
+    @roster
+  end
+  def add_student(student_name, grade_level)
+    @roster[grade_level] ||= []
+    @roster[grade_level] << student_name
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort 
+    a = {}
+    array = [9,10,11,12]
+    array.each do |num|
+      a << @name.grade(num)
+    end
+  end
+end
+
